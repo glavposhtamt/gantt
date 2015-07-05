85c74149b422b9e844fe9c552baf2c07e1021d26
Сергей <glavposhtamt@bk.ru>
2015-07-05 19:16:44 +0300

    Add to GitHub

---
+++ public_html/js/script.js
@@ -0,0 +1,46 @@
+
+ Date.prototype.daysInMonth = function( month ) {
+        return month >= 0 && month < 12 ? 32 - new Date(this.getFullYear(), month, 32).getDate() : null;
+};
+
+window.onload = function() {
+
+    /* Создаём DOM-элементы */
+
+    var content = document.getElementById("year");
+    for( var i = 0; i < 12; ++i ) {
+        var month = document.createElement("div");
+        month.className = "month";
+        content.appendChild(month);      
+    }
+   var months = document.getElementsByClassName("month");
+   
+   for( var i = 0; i < 12; ++i ) {
+        for( var j = 0; j < 5; ++j ) {
+            var week = document.createElement("div");
+            week.className = "week";
+            months[i].appendChild(week);
+        }    
+   }
+
+   var weeks = document.getElementsByClassName("week");
+   
+   var date = new Date();
+   
+   var countDays = 0;
+   
+    for( var i = 0; i < weeks.length; ++i ) {
+        var monthIndex = parseInt( i / 5, 10 );
+        for( var j = 0; j < 7; ++j ) {
+            if( countDays === date.daysInMonth(monthIndex) ) {
+                countDays = 0; break;
+            }
+            var day = document.createElement("div");
+            day.className = "day";
+            weeks[i].appendChild(day);
+            ++countDays;
+        }
+   }
+   
+
+};
